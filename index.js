46e8a0a9ff31df2e238ebce0b8b3c402b885de3f
Mauricio Portillo <Mauricio Portillo@VKMNOTE-070>
2022-01-24 08:55:22 -0300

    adding routes types post & put

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const server = require('./src/app');
 const { conn } = require('./src/db');
-const fetchTypes = require('./src/helpers/fetchTypes');
+// const fetchTypes = require('./src/helpers/fetchTypes');
 const PORT = process.env.PORT || 3001;
 
 conn.sync({ force: false, alter: true })
   .then(() => {
     server.listen(PORT, async () => {
         console.log(`Listening at ${PORT}`)
-        fetchTypes()
+        // fetchTypes()
     });
 });
--- src/routes/types.js
+++ src/routes/types.js
@@ -3,6 +3,25 @@ module.exports = router;
 const { Type } = require('../db.js');
 
 router.get('', async function (req, res) {
-const types = await Type.findAll();
-res.send(types);
+    const types = await Type.findAll();
+    res.send(types);
+});
+
+router.post('', async (req, res) => {
+    const {name} = req.body;
+    await Type.create({
+        name
+    });
+    res.send("Type added correctly")
+});
+
+router.put('/:id', async (req, res) => {
+    const {id} = req.params;
+    const {name} = req.body;
+
+    await Type.update(
+        {name: name},
+        {where: {id:id}}
+    );
+    res.send("ok")
 });
